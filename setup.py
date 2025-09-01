218f154eefe0278c072f636f751ff4768959c205
Divij2601 <divij26dudeja@gmail.com>
2025-09-01 19:41:35 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Medical_chatbot",
+    version="0.1.0",
+    author=" Divij Dudeja",
+    author_email="divijkdudeja@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
