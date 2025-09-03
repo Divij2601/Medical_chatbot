630f21d4c0c5a805aedf768f095fe4560bf8565b
Divij2601 <divij26dudeja@gmail.com>
2025-09-03 23:20:27 +0530

    build the python file for automation

---
+++ src/store_index.py
@@ -0,0 +1,38 @@
+from dotenv import load_dotenv
+import os
+from src.helper import load_pdf_files, filter_to_minimal_documents, text_split, download_embeddings
+from pinecone import Pinecone
+from pinecone import ServerlessSpec
+from langchain_pinecone import PineconeVectorStore
+from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
+load_dotenv()
+
+PINECONE_API_KEY = os.environ.get("PINECONE_API_KEY")
+GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY")
+
+extracted_data = load_pdf_files(data='data/')
+minimal_docs = filter_to_minimal_documents(extracted_data)
+text_chunks = text_split(minimal_docs)
+
+embeddings = download_embeddings()
+
+pinecone_api_key = PINECONE_API_KEY
+pc = Pinecone(api_key=pinecone_api_key)
+
+index_name = "medical-chatbot"
+
+if not pc.has_index(index_name):
+    pc.create_index(
+        name=index_name,
+        dimension=384,  # Dimension of the embeddings
+        metric="cosine",  # Similarity metric
+        spec=ServerlessSpec(cloud="aws", region="us-east-1")
+    )
+
+index = pc.Index(index_name)
+
+doc_search = PineconeVectorStore.from_documents(
+    documents=text_chunks,
+    index_name=index_name,
+    embedding=embeddings,
+)
